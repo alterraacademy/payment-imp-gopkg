ad2ce7f31dc8307e755f7d624f0f31686d0441be
rachmankamil <kamil@alterra.id>
2021-10-27 10:49:27 +0700

    integration update

--- payment.go
+++ payment.go
@@ -2,63 +2,34 @@ package paymentgo
 
 import (
 	canopusgo "github.com/alterraacademy/canopus-gopkg"
+	canopusConfig "github.com/alterraacademy/canopus-gopkg/config"
 )
 
-type Payment struct {
-	Canopus *canopusgo.Canopus
+type PaymentContract interface {
+	GetAvailableMethod(amount float64) ([]PaymentMethod, error)
+	CreateCart(cart CartPayload, method PaymentMethod) (CartResponse, error)
 }
 
-func CreateService() *Payment {
+func InitPayment(paymentType PaymentType, init CanopusService) PaymentContract {
+	if paymentType == Canopus {
+		canopusConfig.DefaultAPIType = canopusConfig.API
+		if init.CanopusType == CanopusSNAP {
+			canopusConfig.DefaultAPIType = canopusConfig.SNAP
+		}
 
-	return &Payment{}
-}
+		paymentClient := canopusgo.NewAPICLient(&canopusgo.ConfigOptions{
+			MerchantKey: init.MerchantKey,
+			MerchantPem: init.MerchantPem,
+			MerchantID:  init.MerchantID,
+			Secret:      init.Secret,
+			Timeout:     int(init.TimeOut),
+		})
 
-func (pay *Payment) InitCanopus(init CanopusService) error {
-	cano, err := canopusgo.CreateService(canopusgo.InitService{
-		Type:        init.CanopusType,
-		MerchantKey: init.MerchantKey,
-		MerchantPem: init.MerchantPem,
-		TimeOut:     init.TimeOut,
-		MerchantID:  init.MerchantID,
-		Secret:      init.Secret,
-	})
+		return &Payment{
+			Canopus: paymentClient,
+		}
 
-	if err != nil {
-		return err
 	}
-
-	pay.Canopus = cano
 
 	return nil
-}
-
-func (pay *Payment) GetAvailableMethod(amount float64) ([]PaymentMethod, error) {
-	var result []PaymentMethod
-
-	// Get from Canopus
-	if pay.Canopus != nil {
-		canoPaymentMethods, err := pay.Canopus.GetAvailableMethod(amount)
-		if err != nil {
-			return []PaymentMethod{}, err
-		}
-		for _, paymentMethod := range canoPaymentMethods {
-			result = append(result, PaymentMethodFromCanopus(paymentMethod))
-		}
-	}
-
-	return result, nil
-}
-
-func (pay *Payment) CreateCart(cart CartPayload, method PaymentMethod) (CartResponse, error) {
-	if pay.Canopus == nil {
-		return CartResponse{}, ErrNoCanopusService
-	}
-
-	cano, err := pay.Canopus.GenerateCart(CartPayloadToCanopus(cart), PaymentMethodToCanopus(method))
-
-	if err != nil {
-		return CartResponse{}, err
-	}
-
-	return CartResponseFromCanopus(cano), nil
 }
